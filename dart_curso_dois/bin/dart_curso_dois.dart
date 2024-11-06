10e53843e6182e4a138e88b85ced041dad58c7bf
Italo61-dev <italosousa42541@gmail.com>
2024-11-05 22:22:57 -0300

    Update dart_curso_dois.dart

--- dart_curso_dois/bin/dart_curso_dois.dart
+++ dart_curso_dois/bin/dart_curso_dois.dart
@@ -5,20 +5,35 @@ void main () {
       String sabor = 'Doce e cítrica';
       int diasDesdeColheita = 40;
       bool isMadura = functEstaMadura(diasDesdeColheita);
+      mostraMadura('Uva', 40, cor: 'roxa');
+}
+/// Posicionais Obrigatórios
+// São a ordem certa dos aguamentos, não posso colocar, a Uva, depois do 40, pois ela foi declarada no parâmetro primeiro o nome.
 
-      print(isMadura);
-      print(functEstaMadura(20));
+/// Nomeados Opcionais
+// são as variáveis entre o conchetes {String? cor}, elas não são obrigatória na invocação da função.
 
-      if(diasDesdeColheita >= 30) {
-        isMadura = true;
-      } else {
-        isMadura = false;
+/// Parâmetros 'Padrão'
+// são o valor atribuído ao nomeados, come se fosse uma variável ex: {String cor = sem cor}, isso vai ficar padrão. caso você não preencha a cor na invocação.
+
+/// Modificador 'required'
+// Deixa o nomeado opcional, obrigatório, e se você não colocar a cor, o código irá da erro. ex: {required String cor}.
+
+void mostraMadura(String nome, int dias, {String? cor}) {
+      if(dias >= 30){
+            print('A $nome está madura');
+      }
+      else {
+            print('A $nome não está madura');
       }
 
+      if(cor != null){
+            print('A $nome é $cor');
+      }
 
 }
 
-bool functEstaMadura(dias){
+bool functEstaMadura(int dias){
       if(dias >= 30){
             return true;
       } else {
