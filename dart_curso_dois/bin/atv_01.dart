93af8e1092ec381271a7820e8393f5c0907a37f8
Ítalo Nascimento <136705319+Italo61-dev@users.noreply.github.com>
2024-11-11 19:30:50 -0300

    Update atv_01.dart

--- dart_curso_dois/bin/atv_01.dart
+++ dart_curso_dois/bin/atv_01.dart
@@ -1,7 +1,9 @@
 main(){
 
-  functInfoFruta('Laranja', 98, 30);
+  Frutas fruta01 = Frutas('Uva', 100.2, 'Roxa', 20, isMadura: true);
+  print(fruta01.);
 }
+
 
 functInfoFruta(String nome, double peso, int colheita){
     int diasMadura = 20;
@@ -13,4 +15,14 @@ functInfoFruta(String nome, double peso, int colheita){
 functAreaRetangulo (double largura, double altura){
   double area = largura * altura;
   return area;
+}
+
+class Frutas{
+  String nome;
+  double peso;
+  String cor;
+  int diasDeColheita;
+  bool? isMadura;
+  
+  Frutas(this.nome, this.peso, this.cor, this.diasDeColheita, {this.isMadura});
 }
