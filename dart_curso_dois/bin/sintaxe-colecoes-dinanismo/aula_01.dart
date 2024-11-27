e7f7cf6f4b913261bd2dd1034d71ff89530b0b31
Ítalo Nascimento <136705319+Italo61-dev@users.noreply.github.com>
2024-11-27 19:39:49 -0300

    Update aula_01.dart

---
+++ dart_curso_dois/bin/sintaxe-colecoes-dinanismo/aula_01.dart
@@ -0,0 +1,18 @@
+void main(){
+  escolherMeioTransporteEnum(Transporte.carro);
+}
+
+void escolherMeioTransporteEnum(Transporte locomocao){
+  if(locomocao == Transporte.carro){
+    print('Vou de CARRO para aventura!');
+  } else if(locomocao == Transporte.bike){
+    print('Vou de BIKE para aventura!');
+  } else {
+    print('Vou para aventura!');
+  }
+}
+
+enum Transporte {
+  carro,
+  bike
+}
