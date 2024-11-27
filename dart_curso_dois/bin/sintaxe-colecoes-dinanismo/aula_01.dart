d26e2506ac8aae474162aa2d8b0834cb4e9ad9d0
Ítalo Nascimento <136705319+Italo61-dev@users.noreply.github.com>
2024-11-27 20:43:44 -0300

    Update aula_01.dart

--- dart_curso_dois/bin/sintaxe-colecoes-dinanismo/aula_01.dart
+++ dart_curso_dois/bin/sintaxe-colecoes-dinanismo/aula_01.dart
@@ -1,18 +1,39 @@
 void main(){
-  escolherMeioTransporteEnum(Transporte.carro);
+  escolherMeioTransporte(Transporte.carro);
+  escolherMeioTransporte(Transporte.onibus);
+  escolherMeioTransporte(Transporte.skate);
 }
 
-void escolherMeioTransporteEnum(Transporte locomocao){
-  if(locomocao == Transporte.carro){
-    print('Vou de CARRO para aventura!');
-  } else if(locomocao == Transporte.bike){
-    print('Vou de BIKE para aventura!');
-  } else {
-    print('Vou para aventura!');
+
+Set<String> registrarDestinos(String destino){
+  Set <String> resgistroVisitados = <String>{};
+  resgistroVisitados.add(destino);
+  return resgistroVisitados;
+
+}
+
+void escolherMeioTransporte(Transporte locomocao){
+
+  switch (locomocao){
+    case Transporte.carro:
+      print('Vou de CARRO para aventura!');
+      break;
+    case Transporte.bike:
+      print('Vou de Bike para aventura!');
+      break;
+    case Transporte.onibus:
+      print('Vou de onibus');
+      break;
+    default:
+      print('Estou indo para aventura');
+      break;
   }
 }
 
 enum Transporte {
   carro,
-  bike
+  bike,
+  onibus,
+  skate
 }
+
