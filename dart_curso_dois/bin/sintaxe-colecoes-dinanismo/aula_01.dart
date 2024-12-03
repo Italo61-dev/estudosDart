4eba56ea236a383297ec2069e1eb393a086e4a75
Ítalo Nascimento <136705319+Italo61-dev@users.noreply.github.com>
2024-12-03 10:58:43 -0300

    Update aula_01.dart

--- dart_curso_dois/bin/sintaxe-colecoes-dinanismo/aula_01.dart
+++ dart_curso_dois/bin/sintaxe-colecoes-dinanismo/aula_01.dart
@@ -1,8 +1,57 @@
+import 'dart:io';
+import 'dart:math';
 void main(){
-  escolherMeioTransporte(Transporte.carro);
-  escolherMeioTransporte(Transporte.onibus);
-  escolherMeioTransporte(Transporte.skate);
-  escolherTipoDeConta(TipoDeContas.black);
+  // escolherMeioTransporte(Transporte.carro);
+  // escolherMeioTransporte(Transporte.onibus);
+  // escolherMeioTransporte(Transporte.skate);
+  // escolherTipoDeConta(TipoDeContas.black);
+  while(true) {
+    print('Escolha uma opção:');
+    print('1: Exibir uma saudação');
+    print('2: Exibir a data e hora atual');
+    print('3: Calcular o quadrado de um número');
+    print('4: Calcular a raiz quadrada de um número');
+    print('5: Encerrar o programa');
+
+    String? escolher = stdin.readLineSync();
+
+
+      switch(escolher){
+        case '1':
+          print('Seja bem vindo');
+          break;
+        case '2':
+          print(DateTime.now());
+          break;
+
+        case '3':
+          print('Digite um número');
+          double? numero = double.tryParse(stdin.readLineSync() ?? '');
+          if(numero != null) {
+            print('O quadradro de $numero é ${numero * numero}');
+          } else {
+            print('Entrada inválida!');
+          }
+          break;
+
+        case '4':
+          print('Digite um número:');
+          double? numero = double.tryParse(stdin.readLineSync() ?? '');
+          if(numero != null) {
+            print('A raiz quadrada de $numero ${sqrt(numero)}');
+          } else {
+            print('Entrada inválida!');
+          }
+          break;
+
+        case'5':
+              print('Encerrar o programa!');
+              return;
+
+        default:
+          print('opção inválida!');
+    }
+  }
 }
 
 void escolherMeioTransporte(Transporte locomocao){
@@ -52,3 +101,6 @@ enum TipoDeContas {
   investimento,
   black
 }
+
+
+
