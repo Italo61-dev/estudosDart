60e91371b76e3b000872af2125758e00a88b21c0
Italo61-dev <italosousa42541@gmail.com>
2024-12-04 00:29:02 -0300

    Create set.dart

---
+++ dart_curso_dois/bin/sintaxe-colecoes-dinanismo/set.dart
@@ -0,0 +1,13 @@
+void main(){
+  Set<String> registrosVisitados = <String>{};
+  registrosVisitados = registrarDestinos('Rio de Janeiro', registrosVisitados);
+  registrosVisitados = registrarDestinos('São Paulo', registrosVisitados);
+  registrosVisitados = registrarDestinos('Rio de Janeiro', registrosVisitados);
+  print(registrosVisitados);
+}
+
+Set<String>registrarDestinos(String destino, Set<String> banco){
+  banco.add(destino);
+  return banco;
+}
+
