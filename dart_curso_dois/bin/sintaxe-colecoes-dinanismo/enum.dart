2962adce24c997efd022bee3eafde579e24e8a59
Ítalo Nascimento <136705319+Italo61-dev@users.noreply.github.com>
2024-12-07 20:59:58 -0300

    Update enum.dart

--- dart_curso_dois/bin/sintaxe-colecoes-dinanismo/enum.dart
+++ dart_curso_dois/bin/sintaxe-colecoes-dinanismo/enum.dart
@@ -6,30 +6,9 @@ void main() {
 
 }
 
-void escolherMeioTransporte(Transporte locomocao){
 
-  switch (locomocao){
-    case Transporte.carro:
-      print('Vou de CARRO para aventura!');
-      break;
-    case Transporte.bike:
-      print('Vou de Bike para aventura!');
-      break;
-    case Transporte.onibus:
-      print('Vou de onibus');
-      break;
-    default:
-      print('Estou indo para aventura');
-      break;
-  }
-}
 
-enum Transporte {
-  carro,
-  bike,
-  onibus,
-  skate
-}
+
 
 
 
