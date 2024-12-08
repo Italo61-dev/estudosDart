c71deff5c1f9aedbd3be26d63ddf15612d18b2b2
Ítalo Nascimento <136705319+Italo61-dev@users.noreply.github.com>
2024-12-07 21:00:06 -0300

    Update set.dart

--- dart_curso_dois/bin/sintaxe-colecoes-dinanismo/set.dart
+++ dart_curso_dois/bin/sintaxe-colecoes-dinanismo/set.dart
@@ -1,9 +1,5 @@
 void main(){
-  Set<String> registrosVisitados = <String>{};
-  registrosVisitados = registrarDestinos('Rio de Janeiro', registrosVisitados);
-  registrosVisitados = registrarDestinos('São Paulo', registrosVisitados);
-  registrosVisitados = registrarDestinos('Rio de Janeiro', registrosVisitados);
-  print(registrosVisitados);
+  
 }
 
 Set<String>registrarDestinos(String destino, Set<String> banco){
