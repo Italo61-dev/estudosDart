e3458f9a090ed9a70d002205f636338bf751130b
Ítalo Nascimento <136705319+Italo61-dev@users.noreply.github.com>
2024-12-07 21:00:14 -0300

    Create viagem.dart

---
+++ dart_curso_dois/bin/sintaxe-colecoes-dinanismo/viagem.dart
@@ -0,0 +1,41 @@
+import 'transporte.dart';
+
+class Viagem{
+  static String codigoTrabalho = "DJNADS12";
+  double dinheiro = 0;
+  Transporte locomocao;
+  Set<String> registrosVisitados = <String>{};
+  Map<String, dynamic> registrarPrecos = {};
+
+  Viagem({required this.locomocao});
+
+  printCodigo(){
+    print(codigoTrabalho);
+  }
+
+  void escolherMeioTransporte(Transporte locomocao){
+
+    switch (locomocao){
+      case Transporte.carro:
+        print('Vou de CARRO para aventura!');
+        break;
+      case Transporte.bike:
+        print('Vou de Bike para aventura!');
+        break;
+      case Transporte.onibus:
+        print('Vou de onibus');
+        break;
+      default:
+        print('Estou indo para aventura');
+        break;
+    }
+  }
+
+  void visitar(String localVisita){
+    registrosVisitados.add(localVisita);
+  }
+
+  voidregistrarPrecoVisita(String local, dynamic preco){
+    registrarPrecos[local] = preco;
+  }
+}
