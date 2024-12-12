b5700052d9f91271f179cc9a7fe227bbfcd261d3
Italo61-dev <italosousa42541@gmail.com>
2024-12-12 19:03:01 -0300

    Update dart_exceptions.dart

--- dart_curso_dois/bin/exception-nullsafety/dart_exceptions.dart
+++ dart_curso_dois/bin/exception-nullsafety/dart_exceptions.dart
@@ -1,25 +1,45 @@
 import 'controllers/bank_controller.dart';
 import 'models/account.dart';
 
-void main() {
-  // Criando o banco
-  BankController bankController = BankController();
+// void main() {
+//   // Criando o banco
+//   BankController bankController = BankController();
+//
+//   // Adicionando contas
+//   bankController.addAccount(
+//       id: "Ricarth",
+//       account:
+//           Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+//
+//   bankController.addAccount(
+//       id: "Kako",
+//       account:
+//           Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+//
+//   // Fazendo transferência
+//   bool result = bankController.makeTransfer(
+//       idSender: "Kako", idReceiver: "Ricarth", amount: 700);
+//
+//   // Observando resultado
+//   print(result);
+// }
 
-  // Adicionando contas
-  bankController.addAccount(
-      id: "Ricarth",
-      account:
-          Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+void main(){
+  print('Started main');
+  functionOne();
+  print('Finished main');
+}
 
-  bankController.addAccount(
-      id: "Kako",
-      account:
-          Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+functionOne(){
+  print('Started F01');
+  functionTwo();
+  print('Finshed F01');
+}
 
-  // Fazendo transferência
-  bool result = bankController.makeTransfer(
-      idSender: "Kako", idReceiver: "Ricarth", amount: 700);
-
-  // Observando resultado
-  print(result);
+functionTwo(){
+  print('Started F02');
+  for(int i = 1; i <= 5; i++){
+    print(i);
+  }
+  print('Finshed F02');
 }
