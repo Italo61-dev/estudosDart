59a9c4b5d257e4b728dce1123d40caa5328543d2
Italo61-dev <italosousa42541@gmail.com>
2024-12-28 15:52:50 -0300

    Update account.dart

--- dart_curso_dois/bin/exception-nullsafety/models/account.dart
+++ dart_curso_dois/bin/exception-nullsafety/models/account.dart
@@ -2,11 +2,13 @@ class Account {
   String name;
   double balance;
   bool isAuthenticated;
+  DateTime? createAt;
 
   Account(
       {required this.name,
       required this.balance,
-      required this.isAuthenticated});
+      required this.isAuthenticated,
+      this.createAt});
 
   editBalance({required value}) {
     balance = balance + value;
