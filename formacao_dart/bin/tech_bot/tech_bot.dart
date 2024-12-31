a6dda3d4ecc1820bd0aef4457ce5ec0913943a77
Italo61-dev <italosousa42541@gmail.com>
2024-12-31 14:43:53 -0300

    Create tech_bot.dart

---
+++ formacao_dart/bin/tech_bot/tech_bot.dart
@@ -0,0 +1,46 @@
+
+import 'dart:io';
+import 'questions/time_questions.dart';
+import 'timing/waiting_time.dart';
+
+void main() async {
+  String techbot = 'TechBOT:\n';
+  var a = true;
+  String usuario = '';
+
+  print('-- Iniciando o TechBOT, aguarde..--');
+
+  await BotClock().clock(2);
+
+  print('TechBOT:\n Oi :) \n Como posso ajudar?');
+
+  do {
+    usuario = stdin.readLineSync().toString();
+    print('-- Processando pergunta, aguarde..--');
+    await BotClock().clock(1);
+
+    if (usuario.contains('xau') ||
+        usuario.contains('Xau') ||
+        usuario.contains('Adeus') ||
+        usuario.contains('adeus')) {
+      a = false;
+      print(techbot + ' Até a proxima!!');
+    } else if (TimeQuestions(usuario).isThisTime()) {
+      // verificar antes, assim não fazemos toda a função sem precisar.
+      await BotClock().clock(2);
+      TimeQuestions(usuario).timeQuestion();
+
+    } else if (false) {
+      //Basta adicionar novas perguntas aqui!
+    } else {
+      await BotClock().clock(2);
+      print(techbot +
+          ' Não fui treinado para responder a essa pergunta \n Desculpe :( ');
+      print(techbot + ' Você pode fazer outra pergunta ou dizer Adeus');
+    }
+  } while (a);
+
+  print('--Encerrando TechBOT--');
+}
+
+
