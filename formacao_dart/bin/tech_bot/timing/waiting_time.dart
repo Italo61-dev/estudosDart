3210924b6b82703d2303603063610066900a31dd
Italo61-dev <italosousa42541@gmail.com>
2025-01-08 01:32:42 -0300

    Update waiting_time.dart

--- formacao_dart/bin/tech_bot/timing/waiting_time.dart
+++ formacao_dart/bin/tech_bot/timing/waiting_time.dart
@@ -3,4 +3,13 @@ class BotClock {
   Future clock(int seconds){
     return Future.delayed(Duration(seconds: seconds));
   }
+  Stream techBotStream(int interval, [int? maxCount]) async*{
+    int i = 1;
+    while (i != maxCount){
+      await Future.delayed(Duration(seconds: interval));
+      yield i++;
+    }
+    print('The Stream is finished');
+  }
 }
+
